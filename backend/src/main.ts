c2f3055627ecc07e36b6b71bbdc822a95ee590af
Victor.Vaglieri <victor-vglr@hotmail.com>
2025-10-09 13:04:15 -0300

    erro

--- backend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient as EstoqueClient } from '@prisma/estoque-client';
-import { PrismaClient as UsuariosClient } from '@prisma/usuarios-client';
-
-const estoque = new EstoqueClient();
-const usuarios = new UsuariosClient();
-
