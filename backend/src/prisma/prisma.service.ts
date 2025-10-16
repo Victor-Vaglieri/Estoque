66df8bc75e2d6e02208b8cc0d3bf0b571f09b8de
Victor-Vaglieri <victor_vglr@hotmail.com>
2025-10-16 15:16:22 -0300

    add função e tabela de avisos, precisa de ajustes porem estruturado

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -5,8 +5,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  // O OnModuleInit é opcional, mas garante que a conexão
-  // seja estabelecida assim que o módulo iniciar.
   async onModuleInit() {
     await this.$connect();
   }
