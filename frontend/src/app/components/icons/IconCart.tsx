67ba97a8608e245683488c106306b86871aed40c
Victor.Vaglieri <victor-vglr@hotmail.com>
2025-10-17 11:25:53 -0300

    começo da tela de cadastrar produto

---
+++ frontend/src/app/components/icons/IconCart.tsx
@@ -0,0 +1,11 @@
+export function IconCart(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 48 48" {...props}><g fill="none">
+            <path fill="#C01554" d="M39 32H13L8 12H44L39 32Z" />
+            <path stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M3 6H6.5L8 12M8 12L13 32H39L44 12H8Z" />
+            <circle cx="13" cy="39" r="3" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" />
+            <circle cx="39" cy="39" r="3" stroke="#000" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" />
+            <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M22 22H30" />
+            <path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" d="M26 26V18" /></g></svg>
+    )
+}
---
+++ frontend/src/app/components/icons/IconGraph.tsx
@@ -0,0 +1,14 @@
+export function IconGraph(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 48 48" {...props}>
+            <g fill="none" strokeWidth="4">
+                <path fill="#C01554" stroke="#000" strokeLinejoin="round" d="M44 5H3.99998V17H44V5Z"/>
+                <path stroke="#000" strokeLinecap="round" strokeLinejoin="round" d="M3.99998 41.0301L16.1756 28.7293L22.7549 35.0301L30.7982 27L35.2786 31.368"/>
+                <path stroke="#000" strokeLinecap="round" d="M44 16.1719V42.1719"/><path stroke="#000" strokeLinecap="round" d="M3.99998 16.1719V30.1719"/>
+                <path stroke="#000" strokeLinecap="round" d="M13.0155 43H44"/>
+                <path stroke="#fff" strokeLinecap="round" d="M17 11H38"/>
+                <path stroke="#fff" strokeLinecap="round" d="M9.99998 10.9966H11"/></g></svg>
+
+    )
+}
+
