205b41b3a726e5267b19fe1a05f3f3e79cda8410
Victor.Vaglieri <victor-vglr@hotmail.com>
2025-10-17 11:55:04 -0300

    correçao dos icones e add de futuros passos para tela de produtos

--- frontend/src/app/components/icons/IconGraph.tsx
+++ frontend/src/app/components/icons/IconGraph.tsx
@@ -1,8 +1,8 @@
 export function IconGraph(props: React.SVGProps<SVGSVGElement>) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 48 48" {...props}>
-            <g fill="none" strokeWidth="4">
-                <path fill="#C01554" stroke="#000" strokeLinejoin="round" d="M44 5H3.99998V17H44V5Z"/>
+            <g fill="none" strokeWidth="2">
+                <path fill="#C73E1F" stroke="#000" strokeLinejoin="round" d="M44 5H3.99998V17H44V5Z"/>
                 <path stroke="#000" strokeLinecap="round" strokeLinejoin="round" d="M3.99998 41.0301L16.1756 28.7293L22.7549 35.0301L30.7982 27L35.2786 31.368"/>
                 <path stroke="#000" strokeLinecap="round" d="M44 16.1719V42.1719"/><path stroke="#000" strokeLinecap="round" d="M3.99998 16.1719V30.1719"/>
                 <path stroke="#000" strokeLinecap="round" d="M13.0155 43H44"/>
